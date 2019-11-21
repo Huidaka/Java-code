e32d8b51fcd0ceb408ce2bba956c68ae7bd01cf3
Huidaka <2585275370@qq.com>
2019-11-21 22:30:35 +0800

    利用位运算符求二进制中一的个数

--- 11_16_02/BinaruySystemQiuYi.java
+++ 11_16_02/BinaruySystemQiuYi.java
@@ -1,20 +1,33 @@
 import java.util.Scanner;;
+//class BinaruySystemQiuYi{
+//    public static void main(String[] args){
+//        int n = 0;
+//        int count = 0;
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        if(n < 0){
+//            count++;
+//        }
+//        while(n/2 != 0){
+//            if(n%2==1 || n%2==-1){
+//                count++;
+//            }
+//            n = n / 2;
+//        }
+//        count++;
+//        System.out.println(count);
+//    }
+//}
 class BinaruySystemQiuYi{
     public static void main(String[] args){
         int n = 0;
         int count = 0;
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        if(n < 0){
+        while(n == 0){
+            n = n & (n-1);
             count++;
         }
-        while(n/2 != 0){
-            if(n%2==1 || n%2==-1){
-                count++;
-            }
-            n = n / 2;
-        }
-        count++;
         System.out.println(count);
-    }   
+    }
 }
