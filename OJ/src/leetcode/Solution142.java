41a6d8977c6e9dfc6fb1502ae7ae7d66f5c18fc8
Huidaka <2585275370@qq.com>
2019-12-15 22:09:27 +0800

    给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。

---
+++ OJ/src/leetcode/Solution142.java
@@ -0,0 +1,24 @@
+package leetcode;
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+public class Solution142 {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
