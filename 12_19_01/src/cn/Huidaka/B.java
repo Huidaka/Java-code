68a51120ea6cc6cd4fa159f75ee4ad90d7e6e33e
Huidaka <2585275370@qq.com>
2019-12-19 23:47:49 +0800

    Java的static初始化

---
+++ 12_19_01/src/cn/Huidaka/B.java
@@ -0,0 +1,30 @@
+package cn.Huidaka;
+
+class A {
+    public A() {
+        System.out.println("1.");
+    }
+    {
+        System.out.println("2.");
+    }
+    static {
+        System.out.println("3.");
+    }
+}
+public class B extends A{
+    public B() {
+        System.out.println("4.");
+    }
+    {
+        System.out.println("5.");
+    }
+    static {
+        System.out.println("6.");
+    } // 367215421548
+    public static void main(String[] args) {
+        System.out.println("7.");
+        new B();
+        new B();
+        System.out.println("8.");
+    }
+}
