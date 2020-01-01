a45ae7266ad86cff5c06d13697b675a0bd602aba
Huidaka <2585275370@qq.com>
2020-01-01 23:03:44 +0800

    深拷贝一个具有随机指针的链表

---
+++ OJ/src/leetcode/Solution138.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val, Node next, Node random) {
+        this.val = val;
+        this.next = next;
+        this.random = random;
+    }
+}
+/**
+ *给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+ * 要求返回这个链表的深拷贝。
+ */
+class Solution138 {
+    public Node copyRandomList(Node head) {
+        if(head == null){
+            return head;
+        }
+        // 空间复杂度O(1)，将克隆结点放在原结点后面
+        Node node = head;
+        // 1->2->3  ==>  1->1'->2->2'->3->3'
+        while(node != null){
+            Node clone = new Node(node.val,node.next,null);
+            Node temp = node.next;
+            node.next = clone;
+            node = temp;
+        }
+        // 处理random指针
+        node = head;
+        while(node != null){
+            // ！！
+            node.next.random = node.random == null ? null : node.random.next;
+            node = node.next.next;
+        }
+        // 还原原始链表，即分离原链表和克隆链表
+        node = head;
+        Node cloneHead = head.next;
+        while(node.next != null){
+            Node temp = node.next;
+            node.next = node.next.next;
+            node = temp;
+        }
+        return cloneHead;
+    }
+}
