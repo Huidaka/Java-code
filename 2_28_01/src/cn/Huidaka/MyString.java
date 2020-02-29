69fa7a61b3d7fbe06fa87c8fa37c4944753d6349
Huidaka <2585275370@qq.com>
2020-02-29 23:42:44 +0800

    实现字符串的相关操作

--- 2_28_01/src/cn/Huidaka/MyString.java
+++ 2_28_01/src/cn/Huidaka/MyString.java
@@ -1,10 +1,10 @@
 package cn.Huidaka;
 
-//字节常用于数据传输以及编码转换的处理之中，String 也能方便的和 byte[] 相互转换
+//实现字符串的相关操作
 
 import java.util.Arrays;
 
-public class StringToBytes {
+public class MyString {
     public static void main(String[] args) {
         String str = "abcdefg";
         char a = str.charAt(1);   //取得指定位置索引字符
