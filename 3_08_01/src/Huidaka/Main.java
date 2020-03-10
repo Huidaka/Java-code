7aac520e54343fe42d53a7a64c5d70df6c6a7620
Huidaka <2585275370@qq.com>
2020-03-10 22:32:14 +0800

    输出字符串的字符集

--- 3_08_01/src/Huidaka/Main.java
+++ 3_08_01/src/Huidaka/Main.java
@@ -7,32 +7,46 @@ import java.util.Scanner;
  * 输出字符串的字符集，去掉重复元素
  */
 class Main{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        Test(str);
+//    }
+//    public static char[] Test(String str){
+//        int flag = 0;
+//        char[] arr1 = str.toCharArray();
+//        char[] arr2 = new char[arr1.length];
+//        arr2[0] = arr1[0];
+//        System.out.print(arr2[0]);
+//        flag++;
+//        for (int i=1; i < arr1.length; i++) {
+//            int j = 0;
+//            for (; j < flag; j++) {
+//                if(arr1[i] == arr2[j]){
+//                    break;
+//                }
+//            }
+//            if(arr1[i] == arr2[j]){
+//                continue;
+//            }
+//            arr2[j] = arr1[i];
+//            flag++;
+//            System.out.print(","+arr1[i]);
+//        }
+//        return arr2;
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        Test(str);
+        String string = sc.nextLine();
+        System.out.println(Test(string));
     }
-    public static char[] Test(String str){
-        int flag = 0;
-        char[] arr1 = str.toCharArray();
-        char[] arr2 = new char[arr1.length];
-        arr2[0] = arr1[0];
-        System.out.print(arr2[0]);
-        flag++;
-        for (int i=1; i < arr1.length; i++) {
-            int j = 0;
-            for (; j < flag; j++) {
-                if(arr1[i] == arr2[j]){
-                    break;
-                }
+    public static StringBuffer Test(String string){
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < string.length(); i++) {
+            if(!stringBuffer.toString().contains(string.charAt(i)+"")){
+                stringBuffer.append(string.charAt(i));
             }
-            if(arr1[i] == arr2[j]){
-                continue;
-            }
-            arr2[j] = arr1[i];
-            flag++;
-            System.out.print(","+arr1[i]);
         }
-        return arr2;
+        return stringBuffer;
     }
 }
