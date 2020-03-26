146fafe1023665dc0757def41958a9da4b6c080a
Huidaka <2585275370@qq.com>
2020-03-26 22:38:39 +0800

    优化了前序遍历和中序遍历建立二叉树

--- OJ/src/leetcode/Solution105.java
+++ OJ/src/leetcode/Solution105.java
@@ -4,34 +4,32 @@ package leetcode;
  * 根据一棵树的前序遍历与中序遍历构造二叉树。
  */
 public class Solution105 {
-    public int pi = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if(preorder == null || inorder == null){
             return null;
         }
-        return  createTree(preorder,inorder, 0, preorder.length);
+        return  createTree(preorder,inorder, 0, inorder.length-1);
 
     }
-
-    public TreeNode createTree(int[] preorder, int[] inorder, int begin ,int end){
-         if(begin > end){
-             return null;
-         }
-         int pinOindex = getIndex(preorder[pi], inorder);
-         TreeNode node = new TreeNode(preorder[pi]);
-         pi++;
-         node.left = createTree(preorder,inorder,begin,pinOindex-1);
-         node.right = createTree(preorder,inorder,pinOindex+1,end);
-         return node;
+    int pi = 0;
+    private TreeNode createTree(int[] preorder, int[] inorder, int begin ,int end){
+        if(begin > end || pi >= preorder.length){
+            return null;
+        }
+        int pinOindex = getIndex(preorder[pi], inorder, begin,end);
+        TreeNode node = new TreeNode(preorder[pi]);
+        pi++;
+        node.left = createTree(preorder,inorder,begin,pinOindex-1);
+        node.right = createTree(preorder,inorder,pinOindex+1,end);
+        return node;
     }
 
-    public int getIndex(int val, int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == val){
+    private int getIndex(int val, int[] arr, int first, int last) {
+        for (int i = last; i >= first; i--) {
+            if (arr[i] == val) {
                 return i;
             }
         }
         return -1;
     }
-
 }
