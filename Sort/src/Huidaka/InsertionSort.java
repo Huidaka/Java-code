8300fbef141f5f333b04b0d5782755838eaddb64
Huidaka <2585275370@qq.com>
2020-03-28 18:26:28 +0800

    插入排序

---
+++ Sort/src/Huidaka/InsertionSort.java
@@ -0,0 +1,35 @@
+package Huidaka;
+
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * 插入排序
+ * 最坏时间复杂度；O(n^2)
+ * 最好的的时间复杂度；O(n)
+ * 特点: 数据越有序，排序速度越快
+ * 排序思想：逐个和前边元素比较，找到比自己小的直接插入
+ */
+public class InsertionSort {
+    public static void insertSort(int[] array) {
+        for (int i = 1, j = 0; i < array.length; i++) {
+            int temp = array[i];
+            for (j = i-1; j >= 0 ; j--) {
+                if(array[j] > temp){
+                    array[j+1] = array[j];
+                }
+                else{
+                    break;
+                }
+            }
+            array[j+1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12,4,6,9,2};
+        insertSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
