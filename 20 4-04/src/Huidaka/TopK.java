fa5f4b1a31d0f2a2e3ffa7532e6d000eab52a72b
Huidaka <2585275370@qq.com>
2020-04-04 16:55:55 +0800

    实现topk问题

---
+++ 20 4-04/src/Huidaka/TopK.java
@@ -0,0 +1,45 @@
+package Huidaka;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 实现topk问题
+ */
+class TopKComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1-o2;
+    }
+}
+
+public class TopK {
+    public Integer[]  findKNum(Integer[] array, int k){
+        TopKComparator topKComparator = new TopKComparator();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue(k,topKComparator);
+        for (int i = 0; i < array.length; i++) {
+            if(priorityQueue.size() < k){
+                priorityQueue.offer(array[i]);
+            }
+            else{
+                if(array[i] > priorityQueue.peek()){
+                    priorityQueue.poll();
+                    priorityQueue.offer(array[i]);
+                }
+            }
+        }
+        Integer[]  ret = new Integer[k];
+        for (int i = 0; i < k; i++) {
+            ret[i] = priorityQueue.poll();
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        TopK topK = new TopK();
+        Integer[]  array = {45,65,5,8,25};
+        System.out.println(Arrays.toString(topK.findKNum(array, 3)));
+
+    }
+}
