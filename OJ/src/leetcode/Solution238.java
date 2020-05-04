6ee8773ded30489c9ec46969cf6ed7e725e80003
Huidaka <2585275370@qq.com>
2020-05-05 00:01:07 +0800

    除自身以外数组的乘积

--- OJ/src/leetcode/Solution238.java
+++ OJ/src/leetcode/Solution238.java
@@ -22,9 +22,3 @@ public class Solution238 {
         return arr;
     }
 }
-
-作者：zhangbaipeng
-        链接：https://leetcode-cn.com/problems/product-of-array-except-self/solution/java-zuo-ji-you-ji-by-zhangbaipeng/
-        来源：力扣（LeetCode）
-        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-}
