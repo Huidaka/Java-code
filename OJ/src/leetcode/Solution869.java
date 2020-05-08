9b9e621b909eada74029fec807f51cb9e6f5b3a8
Huidaka <2585275370@qq.com>
2020-05-08 12:14:26 +0800

    重新排序得到 2 的幂

---
+++ OJ/src/leetcode/Solution869.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 重新排序得到 2 的幂
+ */
+class Solution869 {
+    public boolean reorderedPowerOf2(int N) {
+        int[] A = count(N);
+        for (int i = 0; i < 31; ++i)
+            if (Arrays.equals(A, count(1 << i)))
+                return true;
+        return false;
+    }
+
+    public int[] count(int N) {
+        int[] ans = new int[10];
+        while (N > 0) {
+            ans[N % 10]++;
+            N /= 10;
+        }
+        return ans;
+    }
+}
