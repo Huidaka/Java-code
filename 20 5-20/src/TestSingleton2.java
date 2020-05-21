c1b41c0c83b1e96adc1cc03ad55559654ba39865
Huidaka <2585275370@qq.com>
2020-05-21 16:10:38 +0800

    单例模式之饱汉模式

--- 20 5-20/src/TestSingleton2.java
+++ 20 5-20/src/TestSingleton2.java
@@ -4,13 +4,15 @@ public class TestSingleton2 {
         private Test2() {}
         private static Test2 t = null;
         private static Test2 getTest(){
-            t = new Test2();
+            if(t == null) {
+                t = new Test2();
+            }
             return t;
         }
     }
     public static void main(String[] args) {
-        Test2 t1 = new Test2();
-        Test2 t2 = new Test2();
+        Test2 t1 = Test2.getTest();
+        Test2 t2 = Test2.getTest();
         System.out.println(t1 == t2);
     }
 }
