cf852f2d18fbb2124d621b46d72fabdfbab550bb
Huidaka <2585275370@qq.com>
2020-05-27 23:27:09 +0800

    线程池

--- 20 5-26/src/Huidaka/MyThredPool.java
+++ 20 5-26/src/Huidaka/MyThredPool.java
@@ -11,27 +11,46 @@ public class MyThredPool {
     //保存待执行的任务
     private BlockingQueue<Runnable> queue = new LinkedBlockingDeque<>();
     private int maxThread = 10;
-    private Worker workers[] = null;
+    private Worker workers[] = new Worker[maxThread];
 
     class Worker extends Thread{
         @Override
         public void run() {
             Runnable command = null;
-            try {
-                synchronized (this) {
-                    command = queue.take();
+            while (true) {
+                try {
+                    synchronized (this) {
+                        command = queue.take();
+                    }
+                    command.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                command.run();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
     }
 
-    public void execute(Runnable command){
-        if(maxThread < 10){
+    public void execute(Runnable command) throws InterruptedException {
+        int i = 0;
+        if( i < maxThread){
             Worker worker = new Worker();
             worker.start();
+            workers[i++] = worker;
+            System.out.println("线程");
+        }
+        queue.put(command);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyThredPool myThredPool = new MyThredPool();
+        for (int i= 0 ;i < 10; i++) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("任务");
+                }
+            };
+            myThredPool.execute(runnable);
         }
     }
 
