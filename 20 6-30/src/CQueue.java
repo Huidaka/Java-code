3867f7f985b258997ee2005af98f5239f6c81666
Huidaka <2585275370@qq.com>
2020-06-30 23:29:59 +0800

    两个栈实现一个队列（底层栈用linklist链表）

---
+++ 20 6-30/src/CQueue.java
@@ -0,0 +1,35 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 两个栈实现一个队列（底层栈用linklist链表）
+ */
+class CQueue {
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new LinkedList<Integer>();
+        stack2 = new LinkedList<Integer>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        // 如果第二个栈为空
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()) {
+            return -1;
+        } else {
+            int deleteItem = stack2.pop();
+            return deleteItem;
+        }
+    }
+}
