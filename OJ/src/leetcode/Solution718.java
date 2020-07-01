a3c35a9f879f7541f79e035d198b431f45e78bbc
Huidaka <2585275370@qq.com>
2020-07-01 23:23:28 +0800

    最长重复子数组

---
+++ OJ/src/leetcode/Solution718.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ *最长重复子数组
+ */
+public class Solution718 {
+    public int findLength(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                dp[i][j] = A[i] == B[j] ? dp[i + 1][j + 1] + 1 : 0;
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+
+}
