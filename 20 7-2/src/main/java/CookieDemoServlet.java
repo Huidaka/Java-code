cb39f0f4594d6f0beddeecd3561f17d587a32a2c
Huidaka <2585275370@qq.com>
2020-07-07 00:37:51 +0800

    修改了CookieDemoServlet

--- 20 7-2/src/main/java/CookieDemoServlet.java
+++ 20 7-2/src/main/java/CookieDemoServlet.java
@@ -1,3 +1,5 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +9,20 @@ import java.io.IOException;
 
 public class CookieDemoServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
         String firstname = req.getParameter("firstname");
         String lastname = req.getParameter("lastname");
         resp.setContentType("text/html; charset=utf-8");
-        resp.addCookie(new Cookie("firstnaem", firstname));
-        resp.addCookie(new Cookie("lastnaem", lastname));
+        resp.addCookie(new Cookie("firstname", firstname));
+        resp.addCookie(new Cookie("lastname", lastname));
         resp.getWriter().write("<html>");
-        resp.getWriter().write("数据提交成功");
+        resp.getWriter().write("数据提交成功<br/>");
         resp.getWriter().write("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
     }
 }
