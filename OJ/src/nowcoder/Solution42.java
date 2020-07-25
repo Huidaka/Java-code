71ac10e9773ad9aa6692d7fef010de63f6c65c1c
Huidaka <2585275370@qq.com>
2020-07-25 23:05:08 +0800

    富翁和陌生人计划

---
+++ OJ/src/nowcoder/Solution42.java
@@ -0,0 +1,14 @@
+package nowcoder;
+
+/**
+ * 富翁和陌生人计划
+ */
+public class Solution42 {
+    public static void main(String[] args){
+        int rich = 0;
+        int person = 0;
+        rich = 10*30;
+        person = (int)Math.pow(2,30);
+        System.out.println(rich+ " "+(person-1));
+    }
+}
