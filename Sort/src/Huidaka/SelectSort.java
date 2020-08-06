4eef760f5b9ae3d9bc140a8f7a87ebe2c9051447
Huidaka <2585275370@qq.com>
2020-08-06 23:10:31 +0800

    优化了选择排序

--- Sort/src/Huidaka/SelectSort.java
+++ Sort/src/Huidaka/SelectSort.java
@@ -14,13 +14,15 @@ public class SelectSort {
 
     public void selectSort(int[] array){
         for (int i = 0; i < array.length-1; i++) {
+            int min = i;
             for (int j = i+1; j < array.length; j++) {
-                if(array[j] < array[i]){
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                if(array[j] < array[min]){
+                    min = j;
                 }
             }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
         }
     }
 
