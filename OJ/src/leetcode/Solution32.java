2b27fd3b54c015e733a8c2ecbac75bb4dd0df781
Huidaka <2585275370@qq.com>
2020-08-14 23:57:58 +0800

    长有效括号

---
+++ OJ/src/leetcode/Solution32.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * 最长有效括号
+ */
+public class Solution32 {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
+    public static void main(String[] args) {
+        Solution32 solution32 = new Solution32();
+        System.out.println(solution32.longestValidParentheses("(()"));
+    }
+}
