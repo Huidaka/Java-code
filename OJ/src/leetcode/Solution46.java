f57cf28c605060063e5c28900bfdab632abcace8
Huidaka <2585275370@qq.com>
2020-08-17 23:57:09 +0800

    全排列

---
+++ OJ/src/leetcode/Solution46.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 全排列
+ */
+public class Solution46 {
+    public void backtrack(int n,
+                          ArrayList<Integer> output,
+                          List<List<Integer>> res,
+                          int first) {
+        // 所有数都填完了
+        if (first == n)
+            res.add(new ArrayList<Integer>(output));
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new LinkedList();
+
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        for (int num : nums)
+            output.add(num);
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+}
