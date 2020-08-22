adf7c25ebd302d30997c3f8467c988577abd5b98
Huidaka <2585275370@qq.com>
2020-08-22 22:41:48 +0800

    旋转数组

---
+++ OJ/src/leetcode/Solution189.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 旋转数组
+ */
+public class Solution189 {
+    public void rotate(int[] nums, int k) {
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
+        }
+    }
+}
