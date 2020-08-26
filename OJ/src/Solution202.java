7ce5f2ddb7cfc793e6accfbd65f1b913a599d9f5
Huidaka <2585275370@qq.com>
2020-08-26 22:43:22 +0800

    快乐数

---
+++ OJ/src/Solution202.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *  快乐数
+ */
+public class Solution202 {
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
