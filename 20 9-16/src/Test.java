6495d079ecb7ecd65d0af67368f0fcd474302d7d
Huidaka <2585275370@qq.com>
2020-09-16 22:33:15 +0800

    看了set和map的源码

---
+++ 20 9-16/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        Stack<String> stack = new Stack<>();
+    }
+}
